229f39c0f04e41b2b98a8c1cb5b8cc2f238d9ca7
Josh B <89416369+joshb88@users.noreply.github.com>
2023-10-28 14:44:46 -0500

    implemented inventory class

--- src/entity.cpp
+++ src/entity.cpp
@@ -202,3 +202,33 @@ std::string Staff::getJobTitle() const
 {
     return job_title;
 }
+
+// Inventory Default Constructor
+Inventory::Inventory() : item_name(""), item_count(0), item_threshold(0) {}
+
+// Inventory Parameterized Constructor
+Inventory::Inventory(std::string name, unsigned int count, unsigned int threshold)
+{
+    item_name = name;
+    item_count = count;
+    item_threshold = threshold;
+}
+
+// Inventory Class Setters
+void Inventory::setItemName(std::string name)
+{
+    item_name = name;
+}
+void Inventory::setItemCount(unsigned int count)
+{
+    item_count = count;
+}
+void Inventory::setItemThreshold(unsigned int threshold)
+{
+    item_threshold = threshold;
+}
+
+// Inventory Class Getters
+std::string Inventory::getItemName() const { return item_name; }
+unsigned int Inventory::getItemCount() const { return item_count; }
+unsigned int Inventory::getItemThreshold() const { return item_threshold; }
