3bad2a138e941b8d5066e76b6d2377b7437e1eb3
Joshua Boehm <bhm0814@gmail.com>
2023-11-01 17:00:14 -0500

    update with notes

--- headers/control.h
+++ headers/control.h
@@ -5,3 +5,8 @@
 #include <vector>
 #include "entity.h"
 
+/* Here, we can declare the control classes. These classes will do the computation 
+of the data (entity classes) and consequently should have access to the entity classes.
+These classes are currently developed and being iterated upon.
+DATE WRITTEN: 11-01-23, 4:58pm
+*/
