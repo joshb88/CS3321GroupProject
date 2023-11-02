72ae79b4fe83f28048a95e9533aec86aff64547d
Joshua Boehm <bhm0814@gmail.com>
2023-11-02 00:11:11 -0500

    testing changes

--- main.cpp
+++ main.cpp
@@ -7,36 +7,37 @@
 int main()
 {   
     User newcomer;
+    Room testroom(101, 3, true);
     Patient testPatient;
-    Room R101(101, 5, true);
-    // Patient testPatient("josh","pass", 8884, 19880814, false, "");
+    // Room R101(101, 5, true);
+    // // Patient testPatient("josh","pass", 8884, 19880814, false, "");
 
-    std::cout 
-    << newcomer.getUserLogin() << '\n' 
-    << newcomer.getUserPassword() << std::endl;
+    // std::cout 
+    // << newcomer.getUserLogin() << '\n' 
+    // << newcomer.getUserPassword() << std::endl;
 
-    newcomer.setUserLogin("name-test1");
-    newcomer.setUserPassword("pass-test1");
+    // newcomer.setUserLogin("name-test1");
+    // newcomer.setUserPassword("pass-test1");
 
-    std::cout 
-    << newcomer.getUserLogin() << '\n' 
-    << newcomer.getUserPassword() << std::endl;
-    std::cout << "Hello World. \nThis is a test." << std::endl;
+    // std::cout 
+    // << newcomer.getUserLogin() << '\n' 
+    // << newcomer.getUserPassword() << std::endl;
+    // std::cout << "Hello World. \nThis is a test." << std::endl;
 
-    std::cout
-    << testPatient.getUserLogin() << '\n' << testPatient.getUserPassword() << std::endl;
+    // std::cout
+    // << testPatient.getUserLogin() << '\n' << testPatient.getUserPassword() << std::endl;
 
-    std::cout << testPatient.getDateOfBirth() << std::endl;
-    testPatient.setUserLogin("test-Patient");
-    testPatient.setUserPassword("test-password");
+    // std::cout << testPatient.getDateOfBirth() << std::endl;
+    // testPatient.setUserLogin("test-Patient");
+    // testPatient.setUserPassword("test-password");
 
-    std::cout 
-    << testPatient.getUserLogin() << '\n' 
-    << testPatient.getUserPassword() << std::endl;
+    // std::cout 
+    // << testPatient.getUserLogin() << '\n' 
+    // << testPatient.getUserPassword() << std::endl;
 
-    std::cout << "ty for work" << std::endl;
+    // std::cout << "ty for work" << std::endl;
 
-    newcomer.setUserLogin("can i change this?");
-    std::cout << newcomer.getUserLogin() << std::endl;
+    // newcomer.setUserLogin("can i change this?");
+    // std::cout << newcomer.getUserLogin() << std::endl;
     return 0;
 };
