aa9af4aeb1ae423414925b4462e53b2978cf564b
Josh B <89416369+joshb88@users.noreply.github.com>
2023-11-03 11:24:42 -0500

    configured cmake to creat build files

---
+++ build/CMakeFiles/hospital_management.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for hospital_management.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/mylibrary.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylibrary.dir/src/entity/inventory.cpp.obj"
+  "CMakeFiles/mylibrary.dir/src/entity/inventory.cpp.obj.d"
+  "CMakeFiles/mylibrary.dir/src/entity/patient.cpp.obj"
+  "CMakeFiles/mylibrary.dir/src/entity/patient.cpp.obj.d"
+  "CMakeFiles/mylibrary.dir/src/entity/procedure.cpp.obj"
+  "CMakeFiles/mylibrary.dir/src/entity/procedure.cpp.obj.d"
+  "CMakeFiles/mylibrary.dir/src/entity/room.cpp.obj"
+  "CMakeFiles/mylibrary.dir/src/entity/room.cpp.obj.d"
+  "CMakeFiles/mylibrary.dir/src/entity/schedule.cpp.obj"
+  "CMakeFiles/mylibrary.dir/src/entity/schedule.cpp.obj.d"
+  "CMakeFiles/mylibrary.dir/src/entity/staff.cpp.obj"
+  "CMakeFiles/mylibrary.dir/src/entity/staff.cpp.obj.d"
+  "CMakeFiles/mylibrary.dir/src/entity/user.cpp.obj"
+  "CMakeFiles/mylibrary.dir/src/entity/user.cpp.obj.d"
+  "libmylibrary.a"
+  "libmylibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mylibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
