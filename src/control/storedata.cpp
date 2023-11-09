89dd43e767b107e1689520303147e39492c1ad21
Josh B <89416369+joshb88@users.noreply.github.com>
2023-11-09 15:06:33 -0600

    further testing

--- src/control/storedata.cpp
+++ src/control/storedata.cpp
@@ -30,27 +30,31 @@ void StoreData::storeUser(User& user)
     if (user.getUserLogin() != "") // Assume user's info isn't still default
     {
         std::fstream user_file;
-        user_file.open("./database/users.txt", std::ios::in | std::ios::out | std::ios::app | std::ios::ate); // Open database file
+        user_file.open("./database/users.txt", std::ios::in | std::ios::out | std::ios::app); // Open database file
 
-        if (user_file.is_open()) {                                  // If the file opened correctly,
+        if (user_file.is_open() && user_file.good()) { // If the file opened correctly,
+                              
             std::string userLoginToCheck = user.getUserLogin();
             std::string line;
             bool userExists = false;
 
                                                                     
             while (std::getline(user_file, line)) {                 // Read the file line by line
+                user_file.seekg(0,std::ios::beg);
                 std::istringstream iss(line);                       // Create a stringstream to parse the line
-                std::string firstToken;                             // and a string variable for the first word (user login)
-
-                if (iss >> firstToken) {
-                    if (firstToken == userLoginToCheck) {           // If the User login exists in the file,
-                        userExists = true;                          // Signify that it does exists and
+                std::string firstToken;                             // and a string variable for the first word in each line (user login).
+    
+                if (iss >> firstToken) {                            // If the line has a first token (not an empty line) and
+                    std::cout << firstToken;
+                    if (firstToken == userLoginToCheck) {           // the User login provided is the same as the line,
+                        userExists = true;                          // Signify that the User does exist and
                         break;                                      // stop searching.
                     }
                 }
             }
 
-            if (!userExists) {                                      // If the User login does not exist in the file,
+            if (!userExists && user_file.is_open()) {               // If the User login does not exist in the file,
+                user_file.seekp(0,std::ios::end);
                 user_file <<                                        // write user data as a single line with a newline at the end
                     user.getUserLogin() << " " <<                   
                     user.getUserPassword() << " " << 
