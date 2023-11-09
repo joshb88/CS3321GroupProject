3dab99bc5704341ea966e03082accbb50f3766a1
Josh B <89416369+joshb88@users.noreply.github.com>
2023-11-08 18:44:31 -0600

    preliminary testing on storedata

--- src/control/storedata.cpp
+++ src/control/storedata.cpp
@@ -4,12 +4,25 @@
 
 void StoreData::storeUser(User& user)
 {
-    std::cout << 
-    user.getUserLogin() << '\n' << 
-    user.getUserPassword() << '\n' <<
-    user.getFirstName() << '\n' <<
-    user.getLastName() << '\n' <<
-    user.getDateOfBirth() << '\n' <<
-    user.getGender() <<
-    std::endl;
+    std::fstream user_file;
+    user_file.open("./database/users.txt", std::ios::in | std::ios::out | std::ios::app | std::ios::ate);
+
+    if (user_file.is_open()) 
+    {
+        // Write user data as a single line with a newline at the end
+        user_file << 
+            user.getUserLogin() << " " << 
+            user.getUserPassword() << " " << 
+            user.getLastName() << " " << 
+            user.getFirstName() << " " <<
+            user.getDateOfBirth() << " " <<
+            user.getGender() << " " << std::endl;
+
+        user_file.close();
+        std::cout << "User data written to file." << std::endl;
+    } 
+    else
+    {
+        std::cerr << "Failed to open the file for writing." << std::endl;
+    }
 }
