96252df725fd793d69652d6ac6b4057e930b1585
Josh B <89416369+joshb88@users.noreply.github.com>
2023-12-05 19:13:49 -0600

    idk

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include "entity/schedule.h"
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include "entity/schedule.h"
+#include <ctime>
+#include <chrono>
 #include "boundary/MainMenu.h"
 #include "control/MainMenuLogic.h"
 #include "boundary/patientInterface.h"
@@ -20,14 +22,55 @@ int main()
     Room room(101,5,true);
 
     Schedule schedule("19880814 15:30");
-    schedule.setPatient(patient);
-    schedule.setStaffer(staff);
-    schedule.setProcedure(mri);
-    schedule.setRoom(room);
+    std::cout << schedule.getTime();
+    // schedule.setPatient(patient);
+    // schedule.setStaffer(staff);
+    // schedule.setProcedure(mri);
+    // schedule.setRoom(room);
 
-    // std::cout << schedule.getTime();
+    // std::cout << schedule.getProcedure().getItemsUsed();
 
-    ModifySchedule::writeScheduleToDatabase(schedule);
+    // ModifySchedule::writeScheduleToDatabase(schedule);
+    // std::cout <<
+
+    //     // Get the time
+    //     schedule.getTime() <<
+
+    //     '|' << // Start with general User info
+    //     schedule.getStaffer().getUserLogin() << '-' <<
+    //     schedule.getStaffer().getUserPassword() << '-' <<
+    //     schedule.getStaffer().getLastName() << '-' <<
+    //     schedule.getStaffer().getFirstName() << '-' <<
+    //     std::to_string(schedule.getStaffer().getDateOfBirth()) << '-' <<
+    //     schedule.getStaffer().getGender() << '-' <<
+    //     std::to_string(schedule.getStaffer().getIdNumber()) << '-' <<
+    //     std::to_string(schedule.getStaffer().getClearanceLevel()) << '-' <<
+    //     schedule.getStaffer().getJobTitle() << '-' <<
+    //     schedule.getStaffer().getDateOfHire() <<
+
+    //     '|' << // Store Patient Info
+    //     schedule.getPatient().getUserLogin() << '-' <<
+    //     schedule.getPatient().getUserPassword() << '-' <<
+    //     schedule.getPatient().getLastName() << '-' <<
+    //     schedule.getPatient().getFirstName() << '-' <<
+    //     schedule.getPatient().getDateOfBirth() << '-' <<
+    //     schedule.getPatient().getGender() << '-' <<
+    //     schedule.getPatient().getHasInsurance() << '-' <<
+    //     schedule.getPatient().getInsuranceProvider() << '-' <<
+    //     schedule.getPatient().getHasRoom() << '-' <<
+    //     schedule.getPatient().getRoom().getRoomNumber() << '-' <<
+    //     schedule.getPatient().getRoom().getRoomFloorNumber() << '-' <<
+    //     schedule.getPatient().getRoom().getRoomAvailability() << '-' <<
+
+    //     '|' << // Store Room for Event
+    //     schedule.getRoom().getRoomNumber() << '-' <<
+    //     schedule.getRoom().getRoomFloorNumber() << '-' <<
+    //     schedule.getRoom().getRoomAvailability() << '-' <<
+        
+    //     '|' << // Store Procedure
+    //     schedule.getProcedure().getProcedureName() << '-' <<
+    //     schedule.getProcedure().getCost() << '-' <<
+    //     ModifyProcedure::itemsUsedVectorToString(schedule.getProcedure().getItemsUsed());
 
     return 0;
 };
--- src/control/ModifySchedule.cpp
+++ src/control/ModifySchedule.cpp
@@ -1,62 +1,65 @@
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <ctime>
-#include <chrono>
-#include "entity/schedule.h"
-#include "control/ModifySchedule.h"
-#include "boundary/MainMenu.h"
-#include "control/modifyProcedure.h"
-#include "control/TESTMODINV.h"
+// #include <string>
+// #include <sstream>
+// #include <fstream>
+// #include <iostream>
+// #include "entity/schedule.h"
+// #include <iomanip>
+// #include <ctime>
+// #include <chrono>
+// #include "control/ModifySchedule.h"
+// #include "boundary/MainMenu.h"
+// #include "control/modifyProcedure.h"
+// #include "control/TESTMODINV.h"
 
-void ModifySchedule::writeScheduleToDatabase(Schedule& schedule)
-{
-    std::ofstream schedule_file;
-    schedule_file.open("./database/schedules.txt", std::ios::app);
+// void ModifySchedule::writeScheduleToDatabase(Schedule& schedule)
+// {
+//     std::ofstream schedule_file;
+//     schedule_file.open("./database/schedules.txt", std::ios::app | std::ios::out);
 
-    if (schedule_file.is_open())
-    {
-        schedule_file << 
+//     if (schedule_file.is_open())
+//     {
+//         schedule_file << 
+//         // std::cout <<
 
-        // Get the time
-        schedule.getTime() <<
+//         // Get the time
+//         schedule.getTime() <<
 
-        '|' << // Start with general User info
-        schedule.getStaffer().getUserLogin() << '-' <<
-        schedule.getStaffer().getUserPassword() << '-' <<
-        schedule.getStaffer().getLastName() << '-' <<
-        schedule.getStaffer().getFirstName() << '-' <<
-        std::to_string(schedule.getStaffer().getDateOfBirth()) << '-' <<
-        schedule.getStaffer().getGender() << '-' <<
-        std::to_string(schedule.getStaffer().getIdNumber()) << '-' <<
-        std::to_string(schedule.getStaffer().getClearanceLevel()) << '-' <<
-        schedule.getStaffer().getJobTitle() << '-' <<
-        schedule.getStaffer().getDateOfHire() <<
+//         '|' << // Start with general User info
+//         schedule.getStaffer().getUserLogin() << '-' <<
+//         schedule.getStaffer().getUserPassword() << '-' <<
+//         schedule.getStaffer().getLastName() << '-' <<
+//         schedule.getStaffer().getFirstName() << '-' <<
+//         std::to_string(schedule.getStaffer().getDateOfBirth()) << '-' <<
+//         schedule.getStaffer().getGender() << '-' <<
+//         std::to_string(schedule.getStaffer().getIdNumber()) << '-' <<
+//         std::to_string(schedule.getStaffer().getClearanceLevel()) << '-' <<
+//         schedule.getStaffer().getJobTitle() << '-' <<
+//         schedule.getStaffer().getDateOfHire() <<
 
-        '|' << // Store Patient Info
-        schedule.getPatient().getUserLogin() << '-' <<
-        schedule.getPatient().getUserPassword() << '-' <<
-        schedule.getPatient().getLastName() << '-' <<
-        schedule.getPatient().getFirstName() << '-' <<
-        schedule.getPatient().getDateOfBirth() << '-' <<
-        schedule.getPatient().getGender() << '-' <<
-        schedule.getPatient().getHasInsurance() << '-' <<
-        schedule.getPatient().getInsuranceProvider() << '-' <<
-        schedule.getPatient().getHasRoom() << '-' <<
-        schedule.getPatient().getRoom().getRoomNumber() << '-' <<
-        schedule.getPatient().getRoom().getRoomFloorNumber() << '-' <<
-        schedule.getPatient().getRoom().getRoomAvailability() << '-' <<
+//         '|' << // Store Patient Info
+//         schedule.getPatient().getUserLogin() << '-' <<
+//         schedule.getPatient().getUserPassword() << '-' <<
+//         schedule.getPatient().getLastName() << '-' <<
+//         schedule.getPatient().getFirstName() << '-' <<
+//         schedule.getPatient().getDateOfBirth() << '-' <<
+//         schedule.getPatient().getGender() << '-' <<
+//         schedule.getPatient().getHasInsurance() << '-' <<
+//         schedule.getPatient().getInsuranceProvider() << '-' <<
+//         schedule.getPatient().getHasRoom() << '-' <<
+//         schedule.getPatient().getRoom().getRoomNumber() << '-' <<
+//         schedule.getPatient().getRoom().getRoomFloorNumber() << '-' <<
+//         schedule.getPatient().getRoom().getRoomAvailability() << '-' <<
 
-        '|' << // Store Room for Event
-        schedule.getRoom().getRoomNumber() << '-' <<
-        schedule.getRoom().getRoomFloorNumber() << '-' <<
-        schedule.getRoom().getRoomAvailability() << '-' <<
+//         '|' << // Store Room for Event
+//         schedule.getRoom().getRoomNumber() << '-' <<
+//         schedule.getRoom().getRoomFloorNumber() << '-' <<
+//         schedule.getRoom().getRoomAvailability() << '-' <<
         
-        '|' << // Store Procedure
-        schedule.getProcedure().getProcedureName() << '-' <<
-        schedule.getProcedure().getCost() << '-' <<
-        ModifyProcedure::itemsUsedVectorToString(schedule.getProcedure().getItemsUsed());
-    }
-}
+//         '|' << // Store Procedure
+//         schedule.getProcedure().getProcedureName() << '-' <<
+//         schedule.getProcedure().getCost() << '-' <<
+//         ModifyProcedure::itemsUsedVectorToString(schedule.getProcedure().getItemsUsed());
+//     }
+//     else {std::cout << "file not open";};
+//     schedule_file.close();
+// }
